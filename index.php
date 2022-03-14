1368a42ce5a46659886610abf29923e29aa70c39
ImaevBulat <81855890+ImaevBulat@users.noreply.github.com>
2022-03-14 21:25:09 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Imaev Bulat</title>
+</head>
+<body>
+<<p> Дата и время:
+        <p>
+        <?php
+            $d=date("d.m.Y  H:i");
+  
+  echo($d);
+  
+?>
+<tr>
+<br>
+<br>
+<th align="left">Лабораторная работа №1:</th>
+</tr>
+
+<p> Задания</p>
+<li><a href="lab1-1.php" class="current">Упражнение 1-2</a></li>
+<li><a href="lab1-2.php" class="current">Упражнение 1-3</a></li>
+<li><a href="lab2-5.php" class="current">Упражнение 1-10</a></li>
+<li><a href="lab2-9.php" class="current">Упражнение 1-14</a></li>
+
+<p> Самостоятельная работа</p>
+<ul class="work_menu"> 
+    <li><a href="lab1-5.php" class="current">Задача 1-1</a></li>
+    <li><a href="lab2-10.php" class="current">Задача 1-2</a></li>
+    <li><a href="lab2-11.php" class="current">Задача 1-3</a></li>
+</ul>
+
+<th align="left">Лабораторная работа №2:</th>
+
+<p> Задания</p>
+<li><a href="lab-2-1.php" class="current">Упражнение 2-1</a></li>
+<li><a href="lab-2-2.php" class="current">Упражнение 2-2</a></li>
+<li><a href="lab-2-3.php" class="current">Упражнение 2-3</a></li>
+
+<p> Самостоятельная работа</p>
+<ul class="work_menu"> 
+    <li><a href="lab-2-4.php" class="current">Задача 2-4</a></li>
+    <li><a href="lab-2-4-2.php" class="current">Задача 2-4-2</a></li>
+    <li><a href="lab-2-5.php" class="current">Задача 2-5</a></li>
+    <li><a href="lab-2-5-2.php" class="current">Задача 2-5-2</a></li>
+    <li><a href="lab-2-6-1.php" class="current">Задача 2-6</a></li>
+    <li><a href="lab-2-6-2.php" class="current">Задача 2-6-2</a></li>
+</ul>
+
+    </body>
+</html>
