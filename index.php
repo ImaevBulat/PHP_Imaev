018d259de46439160faa79c2c91ce70278222e57
ImaevBulat <81855890+ImaevBulat@users.noreply.github.com>
2022-03-14 14:25:37 +0500

    Добавил ссылки на задания

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <html>
-<head><meta charset="utf-8"><title>Imaev Bulat</title></head>
-</html>
+<head>
+<meta charset="utf-8">
+<title>Imaev Bulat</title>
+</head>
+<body>
+<tr>
+<th align="left">Лабораторная работа №1:</th>
+</tr>
 <<p> Дата и время:
         <p>
         <?php
             $d=date("d.m.Y  H:i");
   
   echo($d);
+  
 ?>
+<p> Самостоятельная работа</p>
+<ul class="work_menu">
+    <li><a href="lab1-5.php" class="current">Задача 1-1</a></li>
+    <li><a href="lab-2-10.php" class="current">Задача 1-2</a></li>
+    <li><a href="lab-2-11.php" class="current">Задача 1-3</a></li>
+</ul>
+    </body>
+</html>
