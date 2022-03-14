9b08d4890a75880abb67295cf7a7cda0acdeb20f
ImaevBulat <81855890+ImaevBulat@users.noreply.github.com>
2022-03-14 13:59:09 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head><meta charset="utf-8"><title>Imaev Bulat</title></head>
+</html>
+<<p> Дата и время:
+        <p>
+        <?php
+            $d=date("d.m.Y  H:i");
+  
+  echo($d);
+?>
