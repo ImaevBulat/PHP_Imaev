37c8f79061ade680554d2869e83fc339cb7259b6
ImaevBulat <81855890+ImaevBulat@users.noreply.github.com>
2022-03-14 16:31:45 +0500

    Упражнение 1-3

---
+++ lab1-2.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Imaev Bulat</title></head>
+</html>
+<p> Арифметические операции:
+<p>
+<?php
+$x=rand(1,10);
+$y=rand(1,10);
+print ($x . '+' . $y . '=' . ($x+$y) . '<br>');
+print ($x . '-' . $y . '=' . ($x-$y) . '<br>');
+print ($x . '*' . $y . '=' . ($x*$y) . '<br>');
+print ($x . '/' . $y . '=' . ($x/$y) . '<br>');
+?>
