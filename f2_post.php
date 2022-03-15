3091a5ce5e85f54f219af60b75f8388a2a0bb9cb
ImaevBulat <81855890+ImaevBulat@users.noreply.github.com>
2022-03-15 12:45:51 +0500

    Выполнил упражнение 3-2 в ЛР3

---
+++ f2_post.php
@@ -0,0 +1,9 @@
+<html>
+<head><title>Imaev Bulat</title></head>
+</html>
+<?php
+echo ("Hello, " . $_POST["userName_post"]);
+echo ("<hr>");
+echo ("hideField_post is " .
+$_POST["hideField_post"]);
+?>
