143ab4a191b4b62d53097965c521923a1adc8a86
ImaevBulat <81855890+ImaevBulat@users.noreply.github.com>
2022-03-15 12:58:27 +0500

    Выполнил самостоятельное задание 3.6 В-6 в ЛР3

---
+++ s3-6-3.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>Imaev Bulat</title></head>
+</html>
+<p>Вариант 6
+<p> 
+ <?php
+    if (isset($_POST["text1"])){
+        $char = array("а", "е", "ё", "ж", "и", "о", "у", "ы", "э", "ю","я");
+        $string = mb_str_split($_POST["text1"]);
+        $count = strlen($_POST["text1"]);
+        $sum = 0;
+        for ($i = 0; $i <= 10; $i++) {
+            for ($k = 0; $k <= $count; $k++){
+                if ($char[$i] == $string[$k]) $sum++;
+            }
+        }
+    echo ("Количество гласных, входящих в данный текст- ". $sum);   
+    }
+?>
