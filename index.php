9492393f097d09e868f9f41f946801bcfb1f9075
ImaevBulat <81855890+ImaevBulat@users.noreply.github.com>
2022-03-15 12:59:40 +0500

    Отредактировал главную страницу

---
+++ index.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Imaev Bulat</title>
+</head>
+<body>
+<<p> Дата и время:
+        <p>
+        <?php
+            $d=date("d.m.Y  H:i");
+  
+  echo($d);
+  
+?>
+<tr>
+<br>
+<br>
+<th align="left">Лабораторная работа №1:</th>
+</tr>
+
+<p> Задания</p>
+    <li><a href="lab1-1.php" class="current">Упражнение 1-2</a></li>
+    <li><a href="lab1-2.php" class="current">Упражнение 1-3</a></li>
+    <li><a href="lab2-5.php" class="current">Упражнение 1-10</a></li>
+    <li><a href="lab2-9.php" class="current">Упражнение 1-14</a></li>
+
+<p> Самостоятельная работа</p>
+<ul class="work_menu"> 
+        <li><a href="lab1-5.php" class="current">Задача 1-1</a></li>
+        <li><a href="lab2-10.php" class="current">Задача 1-2</a></li>
+        <li><a href="lab2-11.php" class="current">Задача 1-3</a></li>
+</ul>
+
+<th align="left">Лабораторная работа №2:</th>
+
+<p> Задания</p>
+    <li><a href="lab-2-1.php" class="current">Упражнение 2-1</a></li>
+    <li><a href="lab-2-2.php" class="current">Упражнение 2-2</a></li>
+    <li><a href="lab-2-3.php" class="current">Упражнение 2-3</a></li>
+
+<p> Самостоятельная работа</p>
+<ul class="work_menu"> 
+        <li><a href="lab-2-4.php" class="current">Задача 2-4</a></li>
+        <li><a href="lab-2-4-2.php" class="current">Задача 2-4-2</a></li>
+        <li><a href="lab-2-5.php" class="current">Задача 2-5</a></li>
+        <li><a href="lab-2-5-2.php" class="current">Задача 2-5-2</a></li>
+        <li><a href="lab-2-6-1.php" class="current">Задача 2-6</a></li>
+        <li><a href="lab-2-6-2.php" class="current">Задача 2-6-2</a></li>
+</ul>
+
+<th align="left">Лабораторная работа №3:</th>
+<p> Задания</p>
+    <li><a href="f1.html" class="current">Упражнение 3-1</a></li>
+    <li><a href="f2.html" class="current">Упражнение 3-2</a></li>
+    <li><a href="f3.html" class="current">Упражнение 3-3</a></li>
+    <li><a href="f4.html" class="current">Упражнение 3-4</a></li>
+    <li><a href="f5.html" class="current">Упражнение 3-5</a></li>
+    <li><a href="f6.html" class="current">Упражнение 3-6</a></li>
+    <li><a href="f7.php" class="current">Упражнение 3-7</a></li>
+
+<p> Самостоятельная работа</p>
+<ul class="work_menu"> 
+        <li><a href="s3-1.html" class="current">Задача 3.1</a></li>
+        <li><a href="s3-2.php" class="current">Задача 3.2</a></li>
+        <li><a href="s3-3.php" class="current">Задача 3.3</a></li>
+        <li><a href="s3-4.php" class="current">Задача 3.4</a></li>
+        <li><a href="s3-5.php" class="current">Задача 3.5</a></li>
+        <li><a href="s3-6-1.html" class="current">Задача 3.6.1</a></li>
+        <li><a href="s3-6-2.html" class="current">Задача 3.6.2</a></li>
+        <li><a href="s3-6-3.html" class="current">Задача 3.6.3</a></li>
+</ul>
+
+    </body>
+</html>
